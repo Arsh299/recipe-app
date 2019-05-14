0acca32492d0ec9e2affeb32411ecb3df33ee90c
arshspencer <arshspencer.digibrain@gmail.com>
2019-05-14 02:21:28 -0400

    finish application

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -13,16 +13,28 @@ export default class Recipes extends Component {
   state={
     recipes:recipeData,
     search:"",
-    url:`https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`
-  }
+    url:`https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`,
+    base_url:`https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`,
+    query:'&q=',
+    error:""
+  };
 
   async getRecipes(){
     try{
       const data = await fetch(this.state.url);
       const jsonData = await data.json();
-      this.setState({
-        recipes:jsonData.recipes
-      })  
+      if(jsonData.recipes.length===0){
+        this.setState({
+          error:"Sorry but your search did not return any recipes,please try again or press search icon for the most popular recipes"
+        })
+      }
+      else{
+        this.setState({
+          recipes:jsonData.recipes,
+          error:""
+        });  
+      }
+    
     }
     catch(error){
       console.log(error);
@@ -39,13 +51,28 @@ export default class Recipes extends Component {
     });
   };  
   handleSubmit=(e)=>{
-    e.preventDefault()
+    e.preventDefault();
+    const {base_url,query,search} = this.state;
+    this.setState({
+      url:`${base_url}${query}${search}`,
+      search:""
+    },()=>this.getRecipes()
+    );
   }
   render() {
     return (
       <>
         <Search search={this.state.search} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
-        <RecipeList recipes={this.state.recipes}/>
+        {this.state.error ? (<section>
+          <div className="row">
+          <div className="col">
+          <h2 className="text-orange text-center text-uppercase mt-5">
+          {this.state.error}
+          </h2>
+          </div>
+          </div>
+        </section>) : (<RecipeList recipes={this.state.recipes}/>)}
+      
       </>
     )
   }
